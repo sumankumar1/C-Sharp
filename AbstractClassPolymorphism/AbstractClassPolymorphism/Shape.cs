a7a64eafd634441d6b952a3dbb6c3e94db92cf4c
Suman Kumar <37941504+sumankumar1@users.noreply.github.com>
2020-11-25 17:47:28 +0100

    Few Programs

---
+++ AbstractClassPolymorphism/AbstractClassPolymorphism/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassPolymorphism
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+
+        // Must be implemented
+        public abstract double Area();
+    }
+}
---
+++ OOPgame/OOPgame/Warrior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOPgame
+{
+    class Warrior
+    {
+        internal string name;
+        internal double health;
+        internal double maxAttack;
+        internal double maxBlock;
+
+        public Warrior(string name, double health, double maxAttack, double maxBlock)
+        {
+            this.name = name;
+            this.health = health;
+            this.maxAttack = maxAttack;
+            this.maxBlock = maxBlock;
+        }
+
+        internal double Attack()
+        {
+            Random rnd = new Random();
+            return rnd.NextDouble() * maxAttack;
+        }
+
+        internal double Block()
+        {
+            Random rnd = new Random();
+            return rnd.NextDouble() * maxAttack;
+        }
+
+    }
+}
