780668b52f6ed6320ad4f8879f8e805f6f9972d3
Suman Kumar <37941504+sumankumar1@users.noreply.github.com>
2020-12-14 02:49:56 +0100

    Linked List basic: Singly linked list

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinedList<int> singlyLinedList = new SinglyLinedList<int>(0);
+            singlyLinedList.AddFirst(-10);
+            singlyLinedList.AddLast(10);
+            singlyLinedList.AddFirst(-20);
+            singlyLinedList.AddLast(20);
+            singlyLinedList.Print();
+            singlyLinedList.Find(-10);
+            Console.WriteLine("-10 found:{0}", singlyLinedList.found);
+            singlyLinedList.Find(-40);
+            Console.WriteLine("-40 found:{0}", singlyLinedList.found);
+            singlyLinedList.Remove(-10);
+            singlyLinedList.Print();
+        }
+    }
+}
